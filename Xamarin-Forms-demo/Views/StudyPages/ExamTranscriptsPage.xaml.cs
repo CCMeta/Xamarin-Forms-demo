c4e816b0f9ac62225b0c47f903120cc9ff7e9cff
CCMeta <ccmeta@icloud.com>
2021-01-05 14:27:32 +0800

    Add ExamTranscriptsPage

---
+++ Xamarin-Forms-demo/Views/StudyPages/ExamTranscriptsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Xamarin_Forms_demo.Views
+{
+    [DesignTimeVisible(false)]
+    public partial class ExamTranscriptsPage : ContentPage
+    {
+        public ExamTranscriptsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
