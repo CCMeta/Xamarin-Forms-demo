0e5275f9283cb6e3e74563ef23e5ecd3e749b5eb
CCMeta <ccmeta@icloud.com>
2021-01-08 01:54:59 +0800

    Add KnowledgesViewModel in Xamarin

---
+++ Xamarin-Forms-demo/Models/Knowledges.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Xamarin_Forms_demo.Models
+{
+    public class Knowledges
+    {
+        public int id { get; set; }
+        public string topic { get; set; }
+        public string major { get; set; }
+        public string summary { get; set; }
+    }
+}
---
+++ Xamarin-Forms-demo/ViewModels/KnowledgesViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin_Forms_demo.Models;
+using Xamarin_Forms_demo.Services;
+
+namespace Xamarin_Forms_demo.ViewModels
+{
+    public class KnowledgesViewModel : BaseViewModel
+    {
+        private readonly string path = "/api/Knowledges";
+        public ObservableCollection<Knowledges> knowledges = new ObservableCollection<Knowledges>();
+        public ObservableCollection<Knowledges> Knowledges
+        {
+            get { return knowledges; }
+            set
+            {
+                foreach (var item in value)
+                {
+                    knowledges.Insert(0, item);
+                }
+            }
+        }
+        public ICommand GetListCommand { protected set; get; }
+
+        public KnowledgesViewModel() : base()
+        {
+            Title = "Knowledges";
+            GetListCommand = new Command(() =>
+            {
+                GetListAsync();
+            });
+        }
+
+        public async void GetListAsync()
+        {
+            int maxId = Knowledges.Count > 0 ? Knowledges[0].id : 0;
+            var queryParams = new Dictionary<string, string>() {
+                    { "p",maxId.ToString() }
+            };
+            Knowledges = await HttpRequest.GetAsync<ObservableCollection<Knowledges>>(path, queryParams: queryParams);
+            IsBusy = false;
+        }
+
+    }
+}
