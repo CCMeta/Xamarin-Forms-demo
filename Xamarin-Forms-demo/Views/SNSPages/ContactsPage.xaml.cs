41c37b9e5714f118ce6598b0a2d6718aad44469c
CCMeta <ccmeta@icloud.com>
2021-06-17 18:26:18 +0800

    Add ContactsPage UI

--- Xamarin-Forms-demo/Views/SNSPages/ContactsPage.xaml.cs
+++ Xamarin-Forms-demo/Views/SNSPages/ContactsPage.xaml.cs
@@ -11,5 +11,20 @@ namespace Xamarin_Forms_demo.Views
         {
             InitializeComponent();
         }
+        private void OnTypeButtonToggle(object sender, EventArgs e)
+        {
+            //make all box to transparent.
+            BoxView boxView;
+            foreach (var child in listTabNavbar.Children)
+            {
+                boxView = ((StackLayout)child).Children[1] as BoxView;
+                boxView.Color = Color.White;
+            }
+
+            //this is a important thing to get a element in a event just remeber the |as| act
+            boxView = (((Button)sender).Parent as StackLayout).Children[1] as BoxView;
+            Console.WriteLine(boxView.ClassId);
+            boxView.Color = Color.FromHex("#00cccc");
+        }
     }
 }
