2b89f74573a11403ef961065411187a5f9ea0b19
CCMeta <ccmeta@icloud.com>
2021-06-18 22:50:40 +0800

    Fix Bug for previous

--- Xamarin-Forms-demo-api/Models/ContactsRepository.cs
+++ Xamarin-Forms-demo-api/Models/ContactsRepository.cs
@@ -16,7 +16,7 @@ namespace Xamarin_Forms_demo_api.Models
 
         public async Task<IEnumerable<Contacts>> GetList(int uid)
         {
-            string sql = "SELECT * FROM contacts LEFT JOIN users ON users.id = contacts.uid WHERE contacts.uid = @uid";
+            string sql = "SELECT * FROM contacts LEFT JOIN users ON users.id = contacts.partner_id WHERE contacts.uid = @uid";
             return await WithConnection(async conn =>
             {
                 return await conn.QueryAsync<Contacts>(sql, new { uid });
